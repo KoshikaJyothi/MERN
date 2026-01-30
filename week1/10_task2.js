4682753a5947735cf5c998efa79be94de601e0a4
KoshikaJyothi <23eg105q61@anurag.edu.in>
2026-01-30 13:14:58 +0530

    Update 10_task2.js

--- week1/10_task2.js
+++ week1/10_task2.js
@@ -1,20 +1,5 @@
 // Assignment 2: Exam Result Summary
-// ---------------------------------
-// Scenario : Marks are stored subject-wise for a student.
 
-// Test data:
-// const marks = {
-//   maths: 78,
-//   physics: 65,
-//   chemistry: 82,
-//   english: 55
-// };
-
-// /*Tasks:
-//     1. Calculate total marks
-//     2. Calculate average marks
-//     3. Find the highest scoring subject
-//     4. Add a new subject computer: 90*/
 let marks = {
     maths: 78,
     physics: 65,          
